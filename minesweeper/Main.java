313668e7c42e42bec1c46e49684fa3d96518969a
Keri Southwood-Smith <9258461+KSouthwood@users.noreply.github.com>
2020-09-04 23:14:15 -0400

    [feat] complete Stage 4
    Main.java:
    - wrote game loop which finished once all mines are marked
    - make Scanner a class instance
    - new getCell() method to handle the input of the x and y coordinates of the grid
    
    Minefield.java:
    - add new instance fields to keep track of marking mines
    - add boolean[][] to indicate where mines are
    - change some variable and parameter names to avoid conflict with boolean[][] name
    - update all mine checking logic to use boolean[][]
    - new markCell() method to mark/un-mark a cell and update the counters as appropriate
    - new allMinesFound() to validate if all mines have been found
    - update toString() to output field in new format required by stage

--- minesweeper/Main.java
+++ minesweeper/Main.java
@@ -3,9 +3,25 @@ package minesweeper;
 import java.util.Scanner;
 
 public class Main {
+    private static final Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
+        int[] coords;
         Minefield minefield = new Minefield(9, getMines());
+
+        do {
+            System.out.println(minefield.toString());
+            do {
+                coords = getCoordinates();
+                if (minefield.markCell(coords)) {
+                    break;
+                }
+                System.out.println("There is a number here!");
+            } while (true);
+        } while (!minefield.allMinesFound());
+
         System.out.println(minefield.toString());
+        System.out.println("Congratulations! You found all mines!");
     }
 
     /**
@@ -14,17 +30,40 @@ public class Main {
      * @return number of mines
      */
     private static int getMines() {
-        Scanner input = new Scanner(System.in);
         int mines;
 
         do {
-            System.out.println("How many mines do you want on the field? ");
+            System.out.print("How many mines do you want on the field? ");
             mines = input.nextInt();
             if (mines < 1 || mines > 81) {
                 System.out.println("Invalid value. Please enter a number from 1 to 81.");
             }
         } while (mines < 1 || mines > 81);
 
+        input.nextLine();
         return mines;
+    }
+
+    private static int[] getCoordinates() {
+        int[] cell = new int[2];
+
+        do {
+            System.out.print("Set/delete mines marks (x and y coordinates): ");
+            String[] coords = input.nextLine().split(" ");
+            if (coords.length != 2) {
+                System.out.println("Invalid number of arguments!");
+                continue;
+            }
+
+            if (coords[0].matches("[1-9]") && coords[1].matches("[1-9]")) {
+                cell[0] = Integer.parseInt(coords[0]) - 1;
+                cell[1] = Integer.parseInt(coords[1]) - 1;
+            } else {
+                System.out.println("Need to enter two numbers between 1 and 9!");
+                continue;
+            }
+
+            return cell;
+        } while (true);
     }
 }
--- minesweeper/Minefield.java
+++ minesweeper/Minefield.java
@@ -5,8 +5,12 @@ import java.util.Random;
 
 public class Minefield {
     private static Random rnd = null;
-    private final char[][] field;
+    private char[][] field;
+    private boolean[][] mines;
     private static int size;
+    private int minesMarked;
+    private int minesCorrect;
+    private int minesPlaced;
 
     Minefield(int fieldSize) {
         size = fieldSize;
@@ -14,25 +18,32 @@ public class Minefield {
         for (char[] row : field) {
             Arrays.fill(row, '.');
         }
+        mines = new boolean[fieldSize][fieldSize];
+        for (boolean[] row : mines) {
+            Arrays.fill(row, false);
+        }
     }
 
-    Minefield(int fieldSize, int mines) {
+    Minefield(int fieldSize, int numOfMines) {
         this(fieldSize);
         rnd = new Random();
-        populateFieldWithMines(mines);
+        this.minesPlaced = numOfMines;
+        this.minesCorrect = 0;
+        this.minesMarked = 0;
+        populateFieldWithMines(numOfMines);
     }
 
-    private void populateFieldWithMines(int mines) {
-        int mine = 0;
+    private void populateFieldWithMines(int numOfMines) {
+        int minesPlaced = 0;
         do {
             int spot = rnd.nextInt(81);
             int row = spot / size;
             int col = spot % size;
-            if (field[row][col] != 'X') {
-                field[row][col] = 'X';
-                mine++;
+            if (!mines[row][col]) {
+                mines[row][col] = true;
+                minesPlaced++;
             }
-        } while (mine < mines);
+        } while (minesPlaced < numOfMines);
 
         determineNeighbors();
     }
@@ -43,13 +54,13 @@ public class Minefield {
     private void determineNeighbors() {
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
-                if (field[row][col] == 'X') {
+                if (mines[row][col]) {  // don't count the number of mines around a mine
                     continue;
                 }
 
-                int mines = howManyMines(row, col);
-                if (mines > 0) {
-                    field[row][col] = String.valueOf(mines).charAt(0);
+                int numOfMines = howManyMines(row, col);
+                if (numOfMines > 0) {
+                    field[row][col] = String.valueOf(numOfMines).charAt(0);
                 }
             }
         }
@@ -63,29 +74,65 @@ public class Minefield {
      * @return number of mines around specified cell
      */
     private int howManyMines(int cellRow, int cellCol) {
-        int mines = 0;
+        int numOfMines = 0;
         for (int row = Math.max(0, cellRow - 1); row <= Math.min(size - 1, cellRow + 1); row++) {
             for (int col = Math.max(0, cellCol - 1); col <= Math.min(size - 1, cellCol + 1); col++) {
                 if (row == cellRow && col == cellCol) { // don't check the cell we're checking around
                     continue;
                 }
-                if (field[row][col] == 'X') {
-                    mines++;
+                if (mines[row][col]) {
+                    numOfMines++;
                 }
             }
         }
-        return mines;
+        return numOfMines;
+    }
+
+    public boolean markCell(int[] cell) {
+        int row = cell[1];
+        int col = cell[0];
+        if (Character.isDigit(field[row][col])) {
+            return false;
+        }
+
+        if (field[row][col] == '.') {
+            field[row][col] = '*';
+            minesMarked++;
+            if (mines[row][col]) {
+                minesCorrect++;
+            }
+            return true;
+        }
+
+        if (field[row][col] == '*') {
+            field[row][col] = '.';
+            minesMarked--;
+            if (mines[row][col]) {
+                minesCorrect--;
+            }
+            return true;
+        }
+
+        return false; // somethings wrong if we reach here, the field char is something besides a digit, '.' or '*'
+    }
+
+    public boolean allMinesFound() {
+        return minesMarked == minesCorrect && minesCorrect == minesPlaced;
     }
     
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
+        result.append("\n |123456789|\n-|---------|\n");
         for (int row = 0; row < size; row++) {
+            result.append(row + 1);
+            result.append("|");
             for (int col = 0; col < size; col++) {
                 result.append(field[row][col]);
             }
-            result.append('\n');
+            result.append("|\n");
         }
+        result.append("-|---------|");
         return String.valueOf(result);
     }
 }
